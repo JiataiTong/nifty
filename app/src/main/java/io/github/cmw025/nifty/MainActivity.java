9130536755d049db6b66f1536c814aebc544a3df
Jimmy Wei <cmw025@bucknell.edu>
2017-11-05 17:37:58 -0500

    NavigationBar and Sign In

--- app/src/main/java/io/github/cmw025/nifty/MainActivity.java
+++ app/src/main/java/io/github/cmw025/nifty/MainActivity.java
@@ -3,11 +3,34 @@ package io.github.cmw025.nifty;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.tasklist, "Tasks"))
+                .addItem(new BottomNavigationItem(R.drawable.calendar, "Calendar"))
+                .addItem(new BottomNavigationItem(R.drawable.repo, "Projects"))
+                .initialise();
+
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(int position) {
+            }
+            @Override
+            public void onTabUnselected(int position) {
+            }
+            @Override
+            public void onTabReselected(int position) {
+            }
+        });
     }
 }
