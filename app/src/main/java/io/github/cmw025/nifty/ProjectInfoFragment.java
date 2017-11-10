0bb54677bc8bbc188133513bd15ab3c50747460b
WeiweiGu1998 <31387331+WeiweiGu1998@users.noreply.github.com>
2017-11-10 13:31:45 -0500

    Add files via upload
    slidable

---
+++ app/src/main/java/io/github/cmw025/nifty/ProjectInfoFragment.java
@@ -0,0 +1,33 @@
+package io.github.cmw025.nifty;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProjectInfoFragment extends Fragment {
+public int test;
+
+    public ProjectInfoFragment(){
+
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_project_info, container, false);
+
+        return rootView;
+    }
+
+
+}
