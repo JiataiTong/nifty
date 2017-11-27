b0b8a1749a150cb7a022338a23dee94a892dfb34
WeiweiGu1998 <31387331+WeiweiGu1998@users.noreply.github.com>
2017-11-27 16:36:52 -0500

    Add files via upload

--- app/src/main/java/io/github/cmw025/nifty/ProjectInfoFragment.java
+++ app/src/main/java/io/github/cmw025/nifty/ProjectInfoFragment.java
@@ -6,10 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.content.Intent;
 
 public class ProjectInfoFragment extends Fragment {
 public int test;
-
+private ImageButton startChatting;
     public ProjectInfoFragment(){
 
     }
@@ -21,9 +27,20 @@ public int test;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_project_info, container, false);
+        View v=inflater.inflate(R.layout.fragment_project_info,container,false);
+        Intent intent=new Intent(getActivity(),ChattingActivity.class);
+        startChatting = (ImageButton) getActivity().findViewById(R.id.startChatting);
+        startChatting.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View a){
+                startActivity(intent);
+            }
 
-        return rootView;
+        });
+
+
+
+        return v;
     }
 
 
